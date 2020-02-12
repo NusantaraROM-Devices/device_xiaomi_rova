c094e138396c68636700e4467a5a8cacac421ba4
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    rova: Enable freeform windows
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>
    Signed-off-by: TTTT55 <iamtttt55@gmail.com>

--- device.mk
+++ device.mk
@@ -146,6 +146,10 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
