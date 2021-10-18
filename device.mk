8983b28a465aebd8113efb703fa55c994339099e
boedhack99 <boedhack@gmail.com>
2021-10-18 09:48:04 +0700

    rova: Build more PowerHAL * Fix : E vndksupport: Could not load /vendor/lib64/hw/android.hardware.gnss@2.1-impl-qti.so from default namespace: dlopen failed: library android.hardware.power@1.2.so not found: needed by /vendor/lib64/hw/android.hardware.gnss@2.1-impl-qti.so in namespace (default).
    Signed-off-by: boedhack99 <boedhack@gmail.com>

--- device.mk
+++ device.mk
@@ -308,6 +308,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.2 \
+    android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
