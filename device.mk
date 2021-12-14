2d890853c46f6fc3bb03d7eae017dcd4081e57d8
ItoRenz <ridwan.ri214@gmail.com>
2021-12-14 20:58:39 +0700

    rova: Update bluetooth blobs from LA.UM.9.6.3.r1-04400-89xx.0
    * also import Bluetooth (aptX) blobs from Crosshatch - 11/RQ2A.210405.006/7214111
    
    Signed-off-by: ItoRenz <ridwan.ri214@gmail.com>

--- device.mk
+++ device.mk
@@ -66,15 +66,13 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # Bluetooth
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
@@ -92,6 +90,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     GoogleCameraGo
+
+# Component overrides
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # Consumer IR
 PRODUCT_PACKAGES += \
