01e242bf566b057842a5d7a39a5b8f49d1543575
LuK1337 <priv.luk@gmail.com>
2022-01-21 10:57:11 +0100

    rova: RIP com.dsi.ant.antradio_library
    No longer shipped by default with lineage-19.0.
    
    Signed-off-by: ItoRenz <ridwan.ri214@gmail.com>

--- device.mk
+++ device.mk
@@ -14,9 +14,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # ANT
 PRODUCT_PACKAGES += \
-    AntHalService-Soong \
-    com.dsi.ant.antradio_library \
-    com.dsi.ant@1.0.vendor
+    AntHalService-Soong
 
 # APEX
 OVERRIDE_TARGET_FLATTEN_APEX := true
@@ -290,7 +288,6 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
