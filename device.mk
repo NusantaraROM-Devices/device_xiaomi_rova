7f857bfa39a17832f63b7b09dc659f70dddeed14
ItoRenz <ridwan.ri214@gmail.com>
2022-03-26 17:15:52 +0000

    rova: Build android.hardware.radio.config@1.0 from source
    Signed-off-by: ItoRenz <ridwan.ri214@gmail.com>

--- device.mk
+++ device.mk
@@ -371,6 +371,7 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.0 \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor \
