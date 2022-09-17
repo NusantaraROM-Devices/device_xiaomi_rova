9aa0ee60d1b8fbd5ab018192b84be52516fe7a48
Raunak Singh <111221902+Raunak-sdm@users.noreply.github.com>
2022-09-18 00:05:53 +0530

    Update and rename _rova.mk to dot_rova.mk

--- dot_rova.mk
+++ dot_rova.mk
@@ -9,21 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common ConquerOS stuff.
+# Inherit some common dot stuff.
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_SUPPORT_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-CONQUER_BUILD_TYPE := UNOFFICIAL
-# TARGET_USE_GAPPS := false
 
-$(call inherit-product, vendor/conquer/config/common.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from rova device
 $(call inherit-product, device/xiaomi/rova/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rova
-PRODUCT_NAME := conquer_rova
+PRODUCT_NAME := dot_rova
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4A / 5A
