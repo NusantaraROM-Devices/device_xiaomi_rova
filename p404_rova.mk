93f863f3fb08ce07855598abb60630978c45c481
Raunak Singh <111221902+Raunak-sdm@users.noreply.github.com>
2022-09-19 22:16:21 +0530

    Update and rename dot_rova.mk to p404_rova.mk

--- p404_rova.mk
+++ p404_rova.mk
@@ -9,19 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common dot stuff.
+# Inherit some common 404 stuff.
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_SUPPORT_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
-$(call inherit-product, vendor/dot/config/common.mk)
+$(call inherit-product, vendor/404/configs/common.mk)
 
 # Inherit from rova device
 $(call inherit-product, device/xiaomi/rova/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rova
-PRODUCT_NAME := dot_rova
+PRODUCT_NAME := p404_rova
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4A / 5A
